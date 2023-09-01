72b58f5407e395eb1e15272e6a4dd4e40dbfd7a8
Ben <b9008088@newcastle.ac.uk>
2023-09-01 16:41:03 +0100

    Finished challenge

--- 200.py
+++ 200.py
@@ -1,47 +1,67 @@
 from typing import List
 from collections import deque
+import numpy as np
 
 def numIslands(grid: List[List[str]]) -> int:
-    unvisited = []
+    unvisited = deque([])
     visited = {}
-    # Change surrounding numbers
-    def alt_surr(val, y, x):
-        # Mark position as visited
+    def change_surr(y:int, x:int, newval:int):
+        # Check if coord has already been visited
+        if (y,x) in visited:
+            return
+        # Add coord to visited
         visited[(y,x)] = True
-        # Directions of surrounding
-        dir = [
-            [-1, 0],
-            [0, -1],
-            [1, 0],
-            [0, 1]
-        ]
-        # Get all new directions x,y
-        locs = [(int(y)+yadd, int(x)+xadd) for yadd, xadd in dir]
-        # Change values of new directions
-        for ynew, xnew in locs:
-            # Check within bounds of grid
+        # Define all directions
+        dirs = [[-1, 0],[0, -1],[1, 0],[0, 1]]
+        for ydir, xdir in dirs:
+            # Get coords in direction
+            ynew = y + ydir
+            xnew = x + xdir
+            # Check if new coords are on grid
             if (ynew >= 0) and (xnew >= 0) and\
                 (ynew < len(grid)) and (xnew < len(grid[0])):
-                    # Change if island not if ocean or discovered island
-                    if grid[ynew][xnew] == "1":
-                        # Check if position has already been checked
-                        if (ynew, xnew) not in visited:
-                            unvisited.append((ynew, xnew))
-                        grid[ynew][xnew] = str(val)
-    # Initialise starting position
-    nxt = (0, 1)
-    grid[nxt[1]][nxt[0]] = "2"
+                    # Check if new coord is island
+                    if grid[ynew][xnew] != "0":
+                        # Update new coord
+                        grid[ynew][xnew] = newval
+                        # Add new coord to unvisited
+                        unvisited.append((ynew, xnew))
     
-    # Visit each block at a time and check surrounding
-    while True:
-        print(unvisited)
-        alt_surr(2, nxt[1], nxt[0])
-        # Move to next block
-        nxt = unvisited.pop()  
-        if len(unvisited) == 0:
-            break  
+    def grid_where():
+        # Search grid for undiscovered island
+        xlen = len(grid[0])
+        for y in range(len(grid)):
+            for x in range(xlen):
+                if grid[y][x] == "1":
+                    # Return coords
+                    return (y,x)
+        # return no island found
+        return None
 
-    return grid
+    # Store number of islands
+    no_islands = 1
+    # Iterate until all island are found
+    while True:
+        # Find new undiscovered island
+        nxt = grid_where()
+        # If no island found break
+        if nxt is None:
+            break
+        else:
+            # Add new island
+            no_islands += 1
+
+        while True:
+            # Apply analysis to given coords
+            change_surr(nxt[0], nxt[1], no_islands)
+            # Check if unvisited list is empty
+            if len(unvisited) == 0:
+                break  
+            # Get new coords
+            nxt = unvisited.pop()
+
+    return no_islands - 1
+
 
 def disp_grid(grid):
     for y in grid:
@@ -49,10 +69,14 @@ def disp_grid(grid):
             print(x, end=" ")
         print()
 
+
 grid = [
-  ["1","1","1","1","0"],
-  ["1","1","0","1","0"],
-  ["1","1","0","0","0"],
-  ["0","0","0","0","0"]
+  ["0","1","0","0","0"],
+  ["1","1","1","0","0"],
+  ["0","1","1","0","1"],
+  ["0","0","0","0","1"]
 ]
-disp_grid(numIslands(grid))
+print("Grid:")
+disp_grid(grid)
+print()
+print(f"Number of islands: {numIslands(grid)}")
