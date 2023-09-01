3c4b351240a0ce20dea5e228a4a7b7d5c9cbb5ab
Ben <b9008088@newcastle.ac.uk>
2023-09-01 17:37:23 +0100

    Added comments and validation

--- 200.py
+++ 200.py
@@ -3,9 +3,33 @@ from collections import deque
 import numpy as np
 
 def numIslands(grid: List[List[str]]) -> int:
+    '''
+    Returns the number of islands. Islands are disconnected by ocean and islands are connected by cardinally adjacent 1's
+
+            Parameters:
+                    Grid (List[List[str]]): Grid containing island (1) and ocean (0) values
+
+            Returns:
+                    (int): Number of islands
+    '''
+    # Input validation
+    if grid is None:
+        return 0
+
     unvisited = deque([])
     visited = {}
     def change_surr(y:int, x:int, newval:int):
+        '''
+        Changes cardinally adjacent island from given starting coordinates and adds new island values to undiscovered array
+        
+        Parameters:
+                    y (str): y value of starting coordinates
+                    x (str): x value of starting coordinates
+                    newval (str): value to change cardinally adjacent islands to
+
+            Returns:
+                    (int): Number of islands
+        '''
         # Check if coord has already been visited
         if (y,x) in visited:
             return
@@ -28,7 +52,7 @@ def numIslands(grid: List[List[str]]) -> int:
                         unvisited.append((ynew, xnew))
     
     def grid_where():
-        # Search grid for undiscovered island
+        '''Search grid for undiscovered island and returns new coordinates'''
         xlen = len(grid[0])
         for y in range(len(grid)):
             for x in range(xlen):
@@ -50,6 +74,7 @@ def numIslands(grid: List[List[str]]) -> int:
         else:
             # Add new island
             no_islands += 1
+            grid[nxt[0]][nxt[1]] = no_islands
 
         while True:
             # Apply analysis to given coords
@@ -64,19 +89,14 @@ def numIslands(grid: List[List[str]]) -> int:
 
 
 def disp_grid(grid):
+    '''Display grid'''
     for y in grid:
         for x in y:
             print(x, end=" ")
         print()
 
-
-grid = [
-  ["0","1","0","0","0"],
-  ["1","1","1","0","0"],
-  ["0","1","1","0","1"],
-  ["0","0","0","0","1"]
-]
 print("Grid:")
+grid = []
 disp_grid(grid)
 print()
 print(f"Number of islands: {numIslands(grid)}")
